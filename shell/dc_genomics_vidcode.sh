ca5bdf6a05911d74f23130682097ab086de01b2a
robert harbert <rharbert@stonehill.edu>
2021-03-15 22:19:13 -0400

    dc_genomics_vidcode

---
+++ shell/dc_genomics_vidcode.sh
@@ -0,0 +1,22 @@
+mkdir -p ~/dc_workshop/data/untrimmed_fastq/
+cd ~/dc_workshop/data/untrimmed_fastq
+
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/004/SRR2589044/SRR2589044_2.fastq.gz
+
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/003/SRR2584863/SRR2584863_2.fastq.gz
+
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR258/006/SRR2584866/SRR2584866_2.fastq.gz 
+
+gunzip *.fastq.gz # unzip all fastq.gz files one at a time 
+
+# command line fastq quality control
+fastqc -h
+
+mkdir results
+
+fastqc -t 2 -o results data/untrimmed_fastq/*.fastq
+ls results
+
