4ca88708a3818e66658e0d068280861cf5adbb01
robert harbert <rharbert@stonehill.edu>
2021-03-19 21:47:28 -0400

    dc_genomics vidcode pt 3

---
+++ shell/dc_genomics_vidcode_pt3.sh
@@ -0,0 +1,35 @@
+# align reads with bwa
+cd data_process
+curl -L -o ecoli_rel606.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/017/985/GCA_000017985.1_ASM1798v1/GCA_000017985.1_ASM1798v1_genomic.fna.gz
+gunzip ecoli_rel606.fasta.gz
+cd ..
+
+#create results directories
+mkdir results_process
+mkdir -p results_process/sam results_process/bam results_process/vcf
+
+# bwa: create index of reference
+bwa index data_process/ecoli_rel606.fasta
+
+# bwa: align with bwa mem
+bwa mem data_process/ecoli_rel606.fasta \
+  data_process/SRR2584863_1.trim.fastq data_process/SRR2584863_2.trim.fastq \
+  > results_process/sam/SRR2584863.sam
+  
+head results_process/sam/SRR2584863.sam
+
+# automate
+for infile in data_process/*_1.fastq
+do
+  base=$(basename ${infile} _1.fastq)
+  trimmomatic PE -threads 4 data_process/${base}_1.fastq data_process/${base}_2.fastq \
+    data_process/${base}_1.trim.fastq data_process/${base}_1.untrim.fastq \
+    data_process/${base}_2.trim.fastq data_process/${base}_2.untrim.fastq \
+    SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:data_process/NexteraPE-PE.fa:2:40:15
+  
+  bwa mem -t 4 data_process/ecoli_rel606.fasta \
+    data_process/${base}_1.trim.fastq data_process/${base}_2.trim.fastq \
+    > results_process/sam/${base}.sam
+done
+
+
