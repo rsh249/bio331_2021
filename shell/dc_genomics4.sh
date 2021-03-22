3a8c9f5f683da3ca00b539dc38751348930c34ef
robert harbert <rharbert@stonehill.edu>
2021-03-22 15:50:09 -0400

    update class code 3/22

--- shell/dc_genomics4.sh
+++ shell/dc_genomics4.sh
@@ -8,7 +8,6 @@ declare -a sra=("SRR2584863" "SRR2584866" "SRR2589044")
 for SRA in "${sra[@]}"
 do
   echo $SRA
-<<<<<<< HEAD
   #convert to BAM
   samtools view -S -b results_process/sam/$SRA.sam > results_process/bam/$SRA.aligned.bam
   #sort
@@ -32,22 +31,3 @@ done
 
 
 # view vcf files in igv
-=======
-  samtools view -S -b results_process/sam/$SRA.sam > results_process/bam/$SRA.aligned.bam
-  samtools sort -o results_process/bam/$SRA.aligned.sorted.bam results_process/bam/$SRA.aligned.bam 
-  samtools flagstat results_process/bam/$SRA.aligned.sorted.bam
-
-
-
-  bcftools mpileup -O b -o results_process/vcf/$SRA\_raw.bcf \
-    -f ecoli_rel606.fasta results_process/bam/$SRA.aligned.sorted.bam 
-  
-  bcftools call --ploidy 1 -m -v -o results_process/vcf/$SRA\_variants.vcf results_process/vcf/$SRA\_raw.bcf 
-
-
-  vcfutils.pl varFilter results_process/vcf/$SRA\_variants.vcf  > results_process/vcf/$SRA\_final_variants.vcf
-
-  samtools index results_process/bam/$SRA.aligned.sorted.bam
-  #samtools tview results_process/bam/$SRA.aligned.sorted.bam ecoli_rel606.fasta
-done
->>>>>>> 27150d889f41ccff2612e730a34027adce2b1004
