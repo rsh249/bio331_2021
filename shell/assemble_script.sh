07c6ccecfd09f238f725de51993b7f6d794da2a9
robert harbert <rharbert@stonehill.edu>
2021-04-12 15:13:03 -0400

    update assembly_script.sh to add quast assembly analysis

--- shell/assemble_script.sh
+++ shell/assemble_script.sh
@@ -22,7 +22,11 @@ fastqc -t 2 data/unmapped.*.fq
 # run MEGAHIT
 megahit -t 8 -1 data/unmapped.R1.fq -2 data/unmapped.R2.fq  -o dirMEGAHIT
 
+assemblyMH=dirMEGAHIT/final.contigs.fa
+reads1=data/unmapped.R1.fq
+reads2=data/unmapped.R2.fq
 
+quast.py -o quast_output --pe1 $reads1 --pe2 $reads2 --glimmer -t 4 -m 1000 -l "MEGAHIT" $assemblyMH
 
 
 
