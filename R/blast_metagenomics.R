785b6c256e03c17e735a3b137c2a961961a1a5d2
robert harbert <rharbert@stonehill.edu>
2021-04-19 17:17:24 -0400

    blast_metagenomics

---
+++ R/blast_metagenomics.R
@@ -0,0 +1,76 @@
+library(rBLAST)
+library(ggplot2)
+library(parallel)
+library(taxonomizr) # install.packages('taxonomizr') #run if not already installed
+
+# set up a new working directory
+dir.create('metagenomics')
+setwd('metagenomics')
+
+# from: ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral
+dir.create('ref')
+download.file('ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.1.1.genomic.fna.gz', 'ref/viral.1.1.genomic.fna.gz')
+download.file('ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.2.1.genomic.fna.gz', 'ref/viral.2.1.genomic.fna.gz')
+download.file('ftp://ftp.ncbi.nlm.nih.gov/refseq/release/viral/viral.3.1.genomic.fna.gz', 'ref/viral.3.1.genomic.fna.gz')
+
+# unzip and put all of the viral reference files into one:
+system("gunzip ref/*")
+system("cat ref/*.fna > ref/viral_all.fasta")
+
+
+# get our unknown fastq data
+dir.create('data')
+options(timeout=9999) # set a higher timeout for larger downloads
+download.file('https://sra-pub-sars-cov2.s3.amazonaws.com/sra-src/SRR11092062/v300043428_L02_127_1.fq.gz', 'data/viral_metagenome.fq.gz', extra = 'curl')
+system('gunzip data/viral_metagenome.fq.gz')
+
+
+# check for files we need
+list.files(recursive = T)
+
+
+##### BLAST #####
+# read in the query data as a DNAStringSet
+fq <- readDNAStringSet('data/viral_metagenome.fq',format = 'fastq')
+head(fq)
+
+#make a new blast database to search
+reference = 'ref/viral_all.fasta'
+makeblastdb(reference, dbtype = "nucl")
+
+# search the database
+#prepare
+bl <- blast(db=reference, type='blastn')
+
+##### PARALLEL COMPUTING WITH BLAST #####
+#run parallel blast searches
+
+# an R function to run the blast query
+parpredict = function(x){
+  return(predict(bl, x))
+}
+clus = makeCluster('16', type ='FORK');
+splits = clusterSplit(clus, fq)
+p_cl = parLapply(clus, splits, parpredict)
+stopCluster(clus)
+cl = dplyr::bind_rows(p_cl) # bring results back into one dataframe
+##### END PARALLEL COMPUTING ##### 
+
+# set up access to taxonomizr database
+tax_db = '/projectnb/ct-shbioinf/bio331/db/taxonomy' 
+tax_db_files = list.files(tax_db, full.names = TRUE)
+nodes = tax_db_files[grepl('nodes', tax_db_files)]
+names = tax_db_files[grepl('names', tax_db_files)]
+accession = tax_db_files[grepl('accessionTaxa', tax_db_files)]
+taxaNodes<-read.nodes.sql(nodes)
+taxaNames<-read.names.sql(names)
+
+# get data for BLAST hit accession numbers
+accid = as.character(cl$SubjectID) # accession IDs of BLAST hits
+#takes accession number and gets the taxonomic ID
+ids<-accessionToTaxa(accid, accession)
+#taxlist displays the taxonomic names from each ID #
+taxlist=getTaxonomy(ids, taxaNodes, taxaNames)
+cltax=cbind(cl,taxlist)
+
+
--- shell/unicycler_script.sh
+++ shell/unicycler_script.sh
@@ -8,7 +8,7 @@ cd hybrid_assemble
 
 mkdir data
 #set number of threads to use for parallelization
-nthreads=8
+nthreads=16
 #set variables pointing to the SRA run ID for 
 #nanopore data
 nanoraw="SRR5665597"
@@ -35,6 +35,11 @@ unicycler -l data/$nanoraw.fastq -o unicycler_long --threads $nthreads
 unicycler -1 data/$shortraw\_1.fastq -2 data/$shortraw\_2.fastq -l data/$nanoraw.fastq -o unicycler_hybrid --threads $nthreads
 
 conda deactivate
+
+assemblyILL=unicycler_short/assembly.fasta
+assemblyNANO=unicycler_long/assembly.fasta
+assemblyHYB=unicycler_hybrid/read_alignment/all_segments.fasta
+quast.py -o quast_hybrid --pe1 data/$shortraw\_1.fastq --pe2 data/$shortraw\_2.fastq --nanopore data/$nanoraw* --glimmer --threads $nthreads -m 1000 -l "illumina, nanopore, hybrid" $assemblyILL $assemblyNANO $assemblyHYB
 
 
 
